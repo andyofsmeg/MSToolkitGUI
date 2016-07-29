17b225d72d75bda0d7922f3f646bd2bea73094f7
Andy Nicholls <anicholls@Mango.local>
2016-07-29 13:23:34 +0100

    Initialise

---
+++ MSToolkitGUI/ui.R
@@ -0,0 +1,27 @@
+# MSToolkit Trial Simulator
+# See http://mstoolkit.r-forge.r-project.org/ for details of the main package.
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+       sliderInput("bins",
+                   "Number of bins:",
+                   min = 1,
+                   max = 50,
+                   value = 30)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+       plotOutput("distPlot")
+    )
+  )
+))
